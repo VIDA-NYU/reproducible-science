7675af2cac35bd89ca0b2e895acee1cf80802b00
Vicky Steeves <vicky.steeves@nyu.edu>
2017-01-11 17:06:27 -0500

    fixed navigation, fixed logo

--- conf.py
+++ conf.py
@@ -136,14 +136,6 @@ NAVIGATION_LINKS = {
     DEFAULT_LANG: (
         ("/who-we-are", "Who We Are"),
         ("/directory", "Reproducible Resource Directory"),
-	(
-          (
-              ("https://reproduciblescience.org/nyu/", "Reproducibility @ NYU"),
-              ("http://escience.washington.edu/about-us/working-groups/reproducibility-and-open-science/", "Reproducibility @ UW"),
-              ("http://bids.berkeley.edu/working-groups/reproducibility-and-open-science", "Reproducibility @ BIDS"),
-          ),
-          'Reproducibility Initiatives'
-        )
     ),
 }
 
@@ -295,11 +287,11 @@ COMPILERS = {
 # Nikola supports logo display.  If you have one, you can put the URL here.
 # Final output is <img src="LOGO_URL" id="logo" alt="BLOG_TITLE">.
 # The URL may be relative to the site root.
-#LOGO_URL = 'images/logo.png'
+#LOGO_URL = '/images/logo.png'
 
 # If you want to hide the title of your website (for example, if your logo
 # already contains the text), set this to False.
-SHOW_BLOG_TITLE = True
+SHOW_BLOG_TITLE = False
 
 # Writes tag cloud data in form of tag_cloud_data.json.
 # Warning: this option will change its default value to False in v8!
